23d7fe840a1a9c430427fd122a823562adcfab91
amoee-5272 <62556646+amoee-5272@users.noreply.github.com>
2022-01-12 18:52:45 +0800

    Add files via upload

---
+++ final/backend/routes/api/handleCardBook.js
@@ -0,0 +1,25 @@
+import CardBook from '../../models/CardBook';
+const queryCardBook=async()=>{
+    let result= await CardBook.find().sort({name:-1}).limit(100);
+    return result;                                                                                    
+}
+const createCardBook = async(id,name,color)=>{
+    const existing = await CardBook.find({name:name,color:color});
+    if(existing.length!==0) {
+        return false; //已經有存在的
+    }
+    const newCardBook=new CardBook({bookId:id,name:name,color:color,num_of_card:0});
+    newCardBook.save();
+    return true;
+}
+const deleteCardBook = async(id)=>{
+    await CardBook.deleteOne({bookId:id});
+}
+const changeNumOfCard = async(bookId,mod)=>{
+    let target = await CardBook.findOne({bookId:bookId})
+    let num = target[0].num_of_card+mod
+    await CardBook.update({bookId:bookId},{num_of_card:num})
+}
+
+
+export  {queryCardBook,createCardBook,deleteCardBook,changeNumOfCard}
